bcac8bdf197f0c2350c2d496c6d9063e7f2be58d
Muremwa <danmburu254@gmail.com>
2019-03-09 19:02:50 +0300

    added tests for models

---
+++ myhostel/book/tests/test_models.py
@@ -0,0 +1,99 @@
+from django.test import TestCase, tag
+from django.core.files import File
+from django.utils.text import slugify
+
+from mock import MagicMock
+
+# noinspection PyUnresolvedReferences
+from book.models import Hostel, Room, HostelImage, RoomImage
+
+
+@tag('hostel-model')
+class HostelModelTestCase(TestCase):
+    def setUp(self):
+        self.hostel_1 = Hostel(
+            name='The Crud',
+            location='Uplands, Limuru, Kiambu',
+            institution='St. Paul\'s University Main Campus',
+            price_range='KSH 4500 - KSH 6000',
+            distance_from_admin='5000',
+            water=True,
+            electricity=False
+        )
+        self.hostel_1.save()
+        self.image_mock = MagicMock(spec=File)
+        self.image_mock.name = 'new.jpg'
+        self.img_1_1 = HostelImage(
+            hostel=self.hostel_1,
+            file=self.image_mock,
+            is_main=True
+        )
+        self.room_1_1 = Room(
+            room_number='cr-1',
+            house_type='BS',
+            price='5000',
+            hostel=self.hostel_1
+        )
+        self.img_room_1_1 = None
+
+    @tag('basic-hostel')
+    def test_rooms(self):
+        # all rooms and views should be 0
+        self.assertEqual(self.hostel_1.available_rooms, 0)
+        self.assertEqual(self.hostel_1.views, 0)
+        print('rooms and views okay')
+
+    @tag('main-img')
+    def test_main_image(self):
+        # get a main image or default image
+        self.assertEqual(self.hostel_1.get_main_image(), '/media/hostel/default_hostel.jpg')
+        print('default image okay')
+        # create new hostel image and receive it instead of default
+        self.img_1_1.save()
+        self.assertEqual(self.hostel_1.get_main_image(), '/media/hostel/hostel_the-crud.jpg')
+        print('new main image okay')
+        self.img_1_1.delete()
+
+    @tag('room-overall')
+    def test_room_details(self):
+        # hostel should have +1 rooms
+        self.room_1_1.save()
+        self.assertEqual(self.hostel_1.available_rooms, 1)
+        print('new room added')
+
+    @tag('hostel-slug')
+    def test_hostel_slug(self):
+        # make sure the slug is slug-crud
+        self.assertEqual(self.hostel_1.slug, slugify(self.hostel_1.name))
+        print('slugs okay')
+
+    @tag('prices')
+    def test_prices_range(self):
+        # should return [4500, 6000]
+        self.assertListEqual(self.hostel_1.get_prices(), ['4500', '6000'])
+        print('prices okay')
+
+    @tag('url')
+    def test_absolute_url(self):
+        # url for hostel should be /book/hostel/the-crud/
+        self.assertEqual(self.hostel_1.get_absolute_url(), '/book/hostel/the-crud/')
+        # url for room should be /book/hostel/the-crud/cr-1/
+        self.assertEqual(self.room_1_1.get_absolute_url(), '/book/hostel/the-crud/cr-1/')
+        print('url is okay')
+
+    @tag('room-img')
+    def test_room_img(self):
+        # default image should be returned
+        self.room_1_1.save()
+        self.assertEqual(self.room_1_1.get_main_image(), '/media/rooms/default_room.png')
+        print('Default image okay')
+        self.img_room_1_1 = RoomImage(
+            room=self.room_1_1,
+            file=self.image_mock,
+            is_main=True
+        )
+        self.img_room_1_1.save()
+        name = "room_{}.jpg".format(self.room_1_1.room_number)
+        self.assertEqual(self.room_1_1.get_main_image(), '/media/rooms/{}'.format(name))
+        print('New image okay')
+        self.img_room_1_1.delete()
